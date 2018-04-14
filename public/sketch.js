63fd01bda7ca60e250c9321c83aa0196795ecbdc
aprilcoffee <8765498@gmail.com>
2018-04-14 21:34:40 +0800

    update init

---
+++ public/sketch.js
@@ -0,0 +1,26 @@
+var socket;
+var canvas;
+var ellipseSize = 0;
+function setup() {
+ canvas = createCanvas(windowWidth,windowHeight);
+ canvas.style('display','block');
+ background(51);
+ socket = io.connect('10.254.17.39:3000');
+ socket.on('mouse',newDrawing);
+ //frameRate(60);
+}
+function windowResized(){
+    resizeCanvas(windowWidth,windowHeight);
+}
+function newDrawing(data){
+    ellipseSize = data.x;
+}
+function draw() { 
+    background(ellipseSize);
+    stroke(0);
+    fill(0);
+    
+    ellipseSize -= 30;
+//    if(ellipseSize<=0)ellipseSize=0;
+//    ellipse(width/2,height/2,ellipseSize,ellipseSize);
+}
